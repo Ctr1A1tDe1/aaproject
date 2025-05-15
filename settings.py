fe910830522d1bd00230fb46df8aa5133b33745d
Ctr1A1tDe1 <jrbickle@gmail.com>
2025-05-15 21:52:55 +0700

    Alien Invasion V0.1 Learning Test

---
+++ settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    
+    def __init__(self):
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (255, 255, 255)
+        
+        #Ship Settings
+        self.ship_speed = 1.5
+        
+        #Bullets
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
